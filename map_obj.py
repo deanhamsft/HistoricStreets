e4f0eb8bb359a87a07351c61ea04130f9612b1d5
Dean Hatch <dean888@msn.com>
2020-07-08 15:55:16 -0700

    Updates

--- map_obj.py
+++ map_obj.py
@@ -1,8 +1,10 @@
 import pygame
 
 class map_obj():
-    def __init__(self, name, image_path, description, center):
+    def __init__(self, name, image_path, description, center, lat_long_base, lat_long_extremes):
         self.name = name
         self.image = pygame.image.load(image_path)
         self.description = description
         self.center = center
+        self.lat_long_base = lat_long_base
+        self.lat_long_extremes = lat_long_extremes
--- map_placement_tool/map_placement.py
+++ map_placement_tool/map_placement.py
@@ -1,6 +1,11 @@
 import pygame
 import os
-import map_obj
+import importlib.util
+spec = importlib.util.spec_from_file_location("map_obj", "C:/Users/deanha/OneDrive/PythonProjects/Kerry_Boren_project/map_obj.py")
+map_obj = importlib.util.module_from_spec(spec)
+spec.loader.exec_module(map_obj)
+import tkinter as tk
+from tkinter import filedialog
 
 center_offset = [0,0]
 coordinates = [0,0] 
@@ -8,30 +13,23 @@ mouse_pos = [0,0]
 image_dimensions = [0,0]
 held = False
 
-image1 = pygame.image.load('photos/42961479_344104342993622_231747858242469888_n.jpg')
+def open_map():
+    root = tk.Tk()
+    root.withdraw()
 
-picture_1885_locations = [[image1, -370, -940]]
-picture_1917_locations = [0,0]
-picture_1954_locations = [0,0]
-picture_1964_locations = [0,0]
+    file_path = filedialog.askopenfilename()
 
-map_1885 = map_obj.map_obj('1885', 'resources/maps/vernal_1885.jpg',  'Ashley Valley 1885', [-370, -940], [50,30], [2430, 30])
-map_1917 = map_obj.map_obj('1917', 'resources/maps/vernal_1917.jpg',  'Ashley Valley 1917', [-370, -940], [50,30], [2430, 30])
-map_1954 = map_obj.map_obj('1954', 'resources/maps/vernal_1954.jpg',  'Ashley Valley 1954', [-370, -940], [50,30], [2430, 30])
-map_1964 = map_obj.map_obj('1964', 'resources/maps/vernal_1964.jpg',  'Ashley Valley 1964', [-370, -940], [50,30], [2430, 30])
+    return map_obj.map_obj('new map', file_path, 'map area and year description', [0,0],[0,0],[0,0])
 
-def holding():
+def holding(current_map):
     global coordinates, mouse_pos
     temp_work = pygame.mouse.get_pos()
     curr_x = temp_work[0] - mouse_pos[0]
     curr_y = temp_work[1] - mouse_pos[1]
 
-    min_x = 0 - image_dimensions[0] - 1440
-    min_y = 0 - image_dimensions[1] - 900
-
-    max_x = 0 - image_dimensions[0] + 1440
-    max_y = 0 - image_dimensions[1] + 900
-
+    min_x = 0 - current_map.image.get_rect().size[0]
+    min_y = 0 - current_map.image.get_rect().size[1]
+    print('{} | {}'.format(min_x, min_y))
     if  coordinates[0] > min_x and coordinates[0] < 0:
         coordinates[0] += curr_x
     elif curr_x > -1 and coordinates[0] > min_x:
@@ -42,15 +40,13 @@ def holding():
         coordinates[1] -= curr_y
     mouse_pos = pygame.mouse.get_pos()
     
-def game_event_loop(screen):
+def game_event_loop(screen, current_map):
     global held, coordinates, mouse_pos
     if held:
-        holding()
+        holding(current_map)
     screen.fill((0,0,255))
-    screen.blit(map_1885.image, coordinates)
+    screen.blit(current_map.image, coordinates)
 
-    for picture in picture_1885_locations:
-        screen.blit(picture[0], (picture[1], picture[2]))
     pygame.display.flip()
 
     font = pygame.font.Font(None, 24)
@@ -73,12 +69,13 @@ if __name__ == "__main__":
     os.environ['SDL_VIDEO_CENTERED'] = '1'
     pygame.init()
 
+    current_map = open_map()
     screen = pygame.display.set_mode((1440, 900), 0, 32)
-    image_dimensions = map_1885.image.get_rect().size
-    center_offset[0] = image_dimensions[0] - (1440 - (map_1885.center[0]))
-    center_offset[1] = image_dimensions[1] - (900 - (map_1885.center[1]))
+    image_dimensions = current_map.image.get_rect().size
+    center_offset[0] = current_map.center[0]
+    center_offset[1] = current_map.center[1]
     coordinates = [-center_offset[0], -center_offset[1]]
     
     while 1:
-        game_event_loop(screen)
+        game_event_loop(screen, current_map)
         pygame.display.update()
